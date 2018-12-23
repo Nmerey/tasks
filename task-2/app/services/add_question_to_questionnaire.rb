8f2f7a032f08a316e6f1f8de0c018be43db6abe9
Nmerey <merey.nurlan@gmail.com>
2018-12-23 11:21:19 +0100

    Modify AddQuestionToQestionnaire with place variable and checks for it

--- task-2/app/services/add_question_to_questionnaire.rb
+++ task-2/app/services/add_question_to_questionnaire.rb
@@ -1,11 +1,24 @@
 class AddQuestionToQuestionnaire
 	attr_accessor :question
-  def initialize(questionnaire:, question:)
+  def initialize(questionnaire:, question:, place:) #Place added
     @questionnaire = questionnaire
     @question = question
+    @place = place
+    @all_questions = Question.all.where(questionnaire_id: @questionnaire.id)
   end
 
   def call
+  	
+  	if @place.nil?
+  		@place = @all_questions.count
+
+  	elsif @place.to_s.count("a-zA-Z") > 0 #Checks for letters in given place instance
+  		raise ActiveRecord::ActiveRecordError
+
+	elsif @place.negative?
+		raise ActiveRecord::ActiveRecordError 		
+  	end
+  	
   	@question.questionnaire_id = @questionnaire.id
   	@question.save #Need to save question in order to update
     @questionnaire
